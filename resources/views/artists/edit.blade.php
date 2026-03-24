ffebf15380dbb4abae3917b88181a7657df52eef
Jeff Yarnall <jyarnall@imaginesocialstudios.com>
2026-03-24 14:39:01 -0500

    Fixed artist edit to not mess with image positions

--- resources/views/artists/edit.blade.php
+++ resources/views/artists/edit.blade.php
@@ -16,8 +16,13 @@
             </select>
             <x-forms.input class="px-2 py-1 bg-white" type="text" name="url" value="{{ $artist->url }}" />
             <x-forms.input class="px-2 py-1 bg-white" type="text" name="token" value="{{ $artist->token }}" placeholder="API Key" autocomplete="off" />
-            
-            <div x-data="{desktopUseCustom: 0, mobileUseCustom: 0}" class="flex flex-col space-y-2">
+            @php
+                $desktopStyle = $artist->pageStyleForDevice('desktop')->first();
+                $mobileStyle = $artist->pageStyleForDevice('mobile')->first();
+                $desktopUseCustom = $desktopStyle->image_custom_position_x !== null && $desktopStyle->image_custom_position_y !== null;
+                $mobileUseCustom = $mobileStyle->image_custom_position_x !== null && $mobileStyle->image_custom_position_y !== null;
+            @endphp
+            <div x-data="{desktopUseCustom: {{ $desktopUseCustom ? 1 : 0 }}, mobileUseCustom: {{ $mobileUseCustom ? 1 : 0 }}}" class="flex flex-col space-y-2">
                 <div class="flex space-x-2">
                     <x-forms.radio name="desktop[image_use_custom_position]" id="desktop-image-use-custom-position" model="desktopUseCustom">Use Custom Position For Destkop?</x-forms.radio>
                     <x-forms.radio name="mobile[image_use_custom_position]" id="mobile-image-use-custom-position" model="mobileUseCustom">Use Custom Position For Mobile?</x-forms.radio>
@@ -25,22 +30,22 @@
 
                 <x-forms.input class="px-1 py-1 bg-white file:px-1 file:py-0.5 file:rounded file:border file:border-black file:bg-gray-200 hover:file:opacity-60 file:duration-200 cursor-pointer"
                     type="file" name="desktop[image]" />
-                <select x-show="desktopUseCustom == 0" class="px-2 py-1 bg-white border rounded" name="desktop[image_position]">
+                <select x-data="{desktopPosition: '{{ $desktopStyle->image_position }}'}" x-show="desktopUseCustom == 0" x-model="desktopPosition" class="px-2 py-1 bg-white border rounded" name="desktop[image_position]">
                     <x-forms.select-options.object-positions />
                 </select>
 
                 <div x-show="desktopUseCustom == 1" class="">
-                    <x-forms.input name="desktop[image_custom_position_x]" type="number" min="0" max="100" step="1" value="50" />
-                    <x-forms.input name="desktop[image_custom_position_y]" type="number" min="0" max="100" step="1" value="50" />
+                    <x-forms.input name="desktop[image_custom_position_x]" type="number" min="0" max="100" step="1" value="{{ $desktopStyle->image_custom_position_x ?: 50 }}" />
+                    <x-forms.input name="desktop[image_custom_position_y]" type="number" min="0" max="100" step="1" value="{{ $desktopStyle->image_custom_position_y ?: 50 }}" />
                 </div>
                 <x-forms.input class="px-1 py-1 bg-white file:px-1 file:py-0.5 file:rounded file:border file:border-black file:bg-gray-200 hover:file:opacity-60 file:duration-200 cursor-pointer"
                     type="file" name="mobile[image]" />
-                <select x-show="mobileUseCustom == 0" class="px-2 py-1 bg-white border rounded" name="mobile[image_position]">
+                <select x-data="{mobilePosition: '{{ $mobileStyle->image_position }}'}" x-show="mobileUseCustom == 0" x-model="mobilePosition" class="px-2 py-1 bg-white border rounded" name="mobile[image_position]">
                     <x-forms.select-options.object-positions />
                 </select>
                 <div x-show="mobileUseCustom == 1" class="">
-                    <x-forms.input name="mobile[image_custom_position_x]" type="number" min="0" max="100" step="1" value="50" />
-                    <x-forms.input name="mobile[image_custom_position_y]" type="number" min="0" max="100" step="1" value="50" />
+                    <x-forms.input name="mobile[image_custom_position_x]" type="number" min="0" max="100" step="1" value="{{ $mobileStyle->image_custom_position_x ?: 50 }}" />
+                    <x-forms.input name="mobile[image_custom_position_y]" type="number" min="0" max="100" step="1" value="{{ $mobileStyle->image_custom_position_y ?: 50 }}" />
                 </div>
             </div>
             <textarea class="px-2 py-1 bg-white border rounded" placeholder="About" name="about" id="about" cols="30" rows="10">{{ $artist->about }}</textarea>
